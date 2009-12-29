813c15beb962673b2db3822156a5150769cfc4f9
Kuroki Kaze <kurokikaze@yandex.ru>
2009-12-29 14:28:06 +0300

    Disabled Nagle algorithm for connections

--- limestone.js
+++ limestone.js
@@ -6,16 +6,20 @@ sys.puts('Connecting to searchd...');
 var port = 9312;
 
 var server_conn = tcp.createConnection(port);
-server_conn.addListener('connect', function (connection) {
+server_conn.setNoDelay(true);
+server_conn.addListener('connect', function () {
     // Sending protocol version
     sys.puts('Sending version number...');
     // Here we must send 4 bytes, '00000001'
-    server_conn.send(1);
+    server_conn.send(parseInt('00000000',2));
+    server_conn.send(parseInt('00000000',2));
+    server_conn.send(parseInt('00000000',2));
+    server_conn.send(parseInt('00000001',2));
 
     // Waiting for answer
     server_conn.addListener('receive', function(data) {
         sys.puts('Server data received: ' + data);
-        if (data > 0) {
+        if (data.length > 0) {
             // Here is our answer.
             sys.puts('Connection established');
 
