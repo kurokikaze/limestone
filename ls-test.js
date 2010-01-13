8730612e3e449490085e6e5173ef89d1af4f91fe
Kuroki Kaze <kurokikaze@yandex.ru>
2010-01-13 19:42:54 +0300

    Added test file for module

---
+++ ls-test.js
@@ -0,0 +1,15 @@
+var sys = require("sys"),
+   http = require("http"),
+   limestone = require("./limestone");
+http.createServer(function (request, response) {
+  var startDate = (new Date()).getTime();
+  response.sendHeader(200, {"Content-Type": "text/plain"});
+  limestone.connect(9312, function(){
+      limestone.query('test', function(answer){
+        var endDate = (new Date()).getTime();
+        response.sendBody("Hello World\nDone in " + ((endDate-startDate) / 1000) + " seconds\n\n" + JSON.stringify(answer));
+        response.finish();
+      })
+  })
+}).listen(8000);
+sys.puts("Server running at http://127.0.0.1:8000/");
