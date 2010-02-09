f126a1f1a77672497b51a60cda46c2b2c675b16a
Kuroki Kaze <kurokikaze@yandex.ru>
2010-02-09 18:00:01 +0300

    Closing connection after receiving answer in test

--- ls-test.js
+++ ls-test.js
@@ -6,10 +6,11 @@ http.createServer(function (request, response) {
   response.sendHeader(200, {"Content-Type": "text/plain"});
   limestone.connect(9312, function(){
       limestone.query('test', function(answer){
+        limestone.disconnect();
         var endDate = (new Date()).getTime();
         response.sendBody("Hello World\nDone in " + ((endDate-startDate) / 1000) + " seconds\n\n" + JSON.stringify(answer));
         response.finish();
-      })
-  })
+      });
+  });
 }).listen(8000);
 sys.puts("Server running at http://127.0.0.1:8000/");
